b9c13e931a82bc88331a7070e44ad3d76611e9ca
Mirzamurod <mirzamurodrahimberdiyev@gmail.com>
2020-09-15 22:24:28 +0500

    xatola togillandi, 1ta xato qoldi music-editda

---
+++ cf/passport.js
@@ -0,0 +1,34 @@
+const localStrategy = require('passport-local').Strategy;
+const User = require('../model/Users');
+const dbb = require('../cf/db');
+const bcrypt = require('bcryptjs');
+
+module.exports = passport => {
+    passport.use(new localStrategy((username, password, done) => {
+        let a = { username: username };
+        User.findOne(a, (err, user) => {
+            if (err) throw err;
+
+            if (!user)
+                done(null, false, {
+                    message: 'Foydalanuvchi To\'pilmadi!!!'
+                });
+
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) throw err;
+                if (isMatch) done(null, user);
+                else done(null, false, { message: 'Noto\'g\'ri Parol' });
+            });
+        });
+    }));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
+};
---
+++ routes/musics.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const router = express.Router();
+
+// Mucic Model
+const Music = require('../model/Music');
+
+// User Model
+const User = require('../model/Users');
+
+// eA = middle function
+const eA = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        req.flash('danger', 'Iltimos Tizimga Kiring!!!');
+        res.redirect('/login');
+    }
+};
+
+// FOR ADD MUSICS GET
+router.get('/musics/add', eA, (req, res) => {
+    res.render('music_add', {
+        title: "Musiqa Qo'shish"
+    });
+});
+
+// FOR ADD MUSICS POST
+router.post('/musics/add', eA, (req, res) => {
+    req.checkBody('name', "Ism Bo'sh Bo'lmasligi Kerak").notEmpty();
+    //req.checkBody('singer', "Bastakor Bo'sh Bo'lmasligi Kerak").notEmpty();
+    req.checkBody('comment', "Izoh Bo'sh Bo'lmasligi Kerak").notEmpty();
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+        res.render('music_add', {
+            title: "Musiqa Qo'shish",
+            errors: errors
+        });
+    } else {
+        const music = new Music();
+        music.name = req.body.name;
+        music.singer = req.user._id;
+        music.comment = req.body.comment;
+
+        music.save(err => {
+            if (err) {
+                console.log(err);
+            } else {
+                req.flash('success', "Musiqa Muvaffaqiyatli Qo'shildi!!!");
+                res.redirect('/');
+            }
+        });
+    }
+});
+
+// FOR GET BY ID MUSICS GET
+router.get('/musics/:id', eA, (req, res) => {
+    Music.findById(req.params.id, (err, music) => {
+        User.findById(music.singer, (err, user) => {
+            res.render('music', {
+                music: music,
+                singer: user.name
+            });
+        });
+    });
+});
+
+// FOR GET BY ID MUSICS GET
+router.get('/music/edit/:id', eA, (req, res) => {
+    Music.findById(req.params.id, (err, music) => {
+        if (music.singer != req.user._id) {
+            req.flash('danger', 'Haqqingiz Yo\'q!!!');
+            res.redirect('/');
+        }
+        res.render('music_edit', {
+            title: "Musiqani O'zgartirish",
+            music
+        });
+    });
+});
+
+// UPDATE MUSICS
+router.post('/music/edit/:id', eA, (req, res) => {
+    const music = {};
+    music.name = req.body.name;
+    music.singer = req.body.singer;
+    music.comment = req.body.comment;
+
+    const query = { _id: req.params.id };
+
+    Music.updateOne(query, music, err => {
+        if (err) {
+            console.log(err);
+        } else {
+            req.flash('success', "Musiqa Muvaffaqiyatli O'zgartirildi!!!");
+            res.redirect('/');
+        }
+    });
+});
+
+// FOR GET BY ID MUSICS GET
+router.delete('/musics/:id', eA, (req, res) => {
+    if (!req.user._id) res.status(500).send();
+
+    let a = { _id: req.params.id };
+
+    Music.findById(req.params.id, (err, music) => {
+        if (music.singer != req.user._id) res.status(500).send();
+        else {
+            Music.deleteOne(a, err => {
+                if (err) console.log(err);
+
+                res.send('Success');
+            });
+        }
+    });
+});
+
+module.exports = router;
