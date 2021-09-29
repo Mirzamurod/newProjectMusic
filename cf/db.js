d5b2d0a873bd5e7a543fec2bd0c7cbce7fa1fbc1
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2021-09-29 23:13:10 +0500

    changed

--- cf/db.js
+++ cf/db.js
@@ -1,4 +1,18 @@
-module.exports = {
-    db: 'mongodb://localhost:27017/new',
-    secret: 'maxfiysoz'
-};
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb+srv://Mirzamurod:microlab1M@musicproject.rxwqn.mongodb.net/new-music-project?retryWrites=true&w=majority', {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+        })
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        console.error(`Error: ${error.message}`.red.underline.bold)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
